8e632c4dfd10ce85b16d8497bd9febfa493a52f8
shuishouy <yjtpost@qq.com>
2024-05-05 14:33:26 +0800

    add plugins: hop, lsp_signature

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -14,12 +14,12 @@ keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 keymap.set("v", "<C-/>", "gb")
 
 -- ---------- 正常模式 ---------- ---
-keymap.set("n", "J", "") -- delete the keymap of 'J'
+-- keymap.set("n", "J", "") -- delete the keymap of 'J'
 -- 窗口
 -- keymap.set("n", "<leader>sv", "<C-w>v") -- 垂直复制窗口 
 -- keymap.set("n", "<leader>sh", "<C-w>s") -- 水平复制窗口
 keymap.set("n", "<leader>sc", "<C-w>c") -- 关闭分窗口
-keymap.set("n", "<leader>x", "<cmd>bd<CR>") -- 关闭窗口
+keymap.set("n", "<leader>x", "<cmd>bd!<CR>") -- 关闭窗口
 keymap.set("n", "<leader>tm", "<C-w>s<cmd>term<CR>") -- 水平新增终端
 keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>") -- toggle nvim-tree
 -- 文件操作
@@ -37,7 +37,7 @@ keymap.set("n", "<A-.>", "<C-w><")
 keymap.set("n", "<A-- >", "<C-w>-")
 keymap.set("n", "<A-= >", "<C-w>+")
 -- build and run
--- keymap.set("n", "<leader>bu", "<C-w>v<cmd>term g++ *.cpp<CR>")
+keymap.set("n", "<leader>bu", "<C-w>v<cmd>term cd build; cmake ..<CR>")
 keymap.set("n", "<leader>R", "<cmd>w<CR> <C-w>s <cmd>term cd build; make; ./a.out<CR>")
 keymap.set("n", "<leader>rr", "<cmd>w<CR> <C-w>v <cmd>term cd build; make; ./a.out<CR>")
 -- dashboard 
@@ -53,16 +53,6 @@ keymap.set("n", "<leader>pks", "<cmd>PackerSync<CR>")
 -- keymap.set("n", "<leader>=", "ggVG=")
 
 
---打开终端后自动进入插入模式
-local term_mode = vim.api.nvim_create_augroup("TERM_MODE", {clear = true})
-vim.api.nvim_create_autocmd({"TermOpen"}, {
-    pattern = "*",
-    group = term_mode,
-    command = [[normal i]]
-})
--- 从终端模式变为普通模式
-
-keymap.set("t", "<M-t>", "<C-\\><C-n>:bd!<CR>",{silent = true})
 -- plugin of 'runcode'
 -- vim.keymap.set('n', '<leader>rr', ':RunCode<CR>', { noremap = true, silent = false })
 -- vim.keymap.set('n', '<leader>rf', ':RunFile<CR>', { noremap = true, silent = false })
@@ -76,7 +66,7 @@ keymap.set("t", "<M-t>", "<C-\\><C-n>:bd!<CR>",{silent = true})
 keymap.set("n", "<leader>cs", '<cmd>Themery<CR>', {silent = true})
 
 -- ------------------ hop ----------------------
-keymap.set("n", "F", "<cmd>HopWord<CR>", {silent = true})
+keymap.set("n", "<C-f>", "<cmd>HopWord<CR>", {silent = true})
 -- keymap.set("n", "fc", "<cmd>HopChar1<CR>", {silent = true})
 
 -- ------------------ folding parentheses ---------------
@@ -86,3 +76,6 @@ keymap.set("n", "<leader>fo", "<cmd>foldopen<CR>", {silent = true})
 -- plugin: bufferline
 keymap.set("n", "K", "<cmd>BufferLineCyclePrev<CR>", {silent = true})
 keymap.set("n", "J", "<cmd>BufferLineCycleNext<CR>", {silent = true})
+
+-- format c/c++ code
+-- keymap.set("n", "<leader>fm", "<cmd>term clang-format -style=file -i main.cc<CR>k", {silent = true})
